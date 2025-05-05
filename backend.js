cc268ef683ee2a8e618872a6a214f9f3dd11ebbb
boodyxdgamer <65690564+boodyxdgamer@users.noreply.github.com>
2025-05-06 02:36:26 +0300

    cleaned up css and headers in all htmls added dark mode features to contact us created an all products page made a js file containing all used java scripts (for dark mode and search only

---
+++ backend.js
@@ -0,0 +1,48 @@
+ 
+    // dark theme js
+
+    const toggleBtn = document.getElementById('theme');
+    
+// Check for saved user preference, if any, on page load
+if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+}
+
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    
+    // Save user preference
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
+         // search bar js
+         document.getElementById("srch").addEventListener("focus",function(){
+            document.getElementById("srchlist").style.display="block";
+        });
+        document.addEventListener("click",function(event){
+            if(!event.target.closest(".srchform")){
+                document.getElementById("srchlist").style.display="none";
+            }
+        });
+        function search(){
+            let input = document.getElementById("srch").value.toLowerCase();
+            let items = document.getElementById("srchlist").getElementsByTagName("li");
+    
+            
+            for(let i=0; i< items.length; i++){
+                let text= items[i].innerText.toLowerCase();
+                if(text.includes(input)){
+                    items[i].style.display="block";
+                }else{
+                    items[i].style.display="none";
+                }
+            }
+        }
+        function redirectToPage(page){
+            window.location.href =page;
+    
+        }
+    
